b91b48d93245177a68ee24af97669e4fc78a3a84
Gronkor <maff0004@gmail.com>
2017-01-13 22:22:16 -0600

    will this fix it?
    pls?

--- Assets/cameraFollow.cs
+++ Assets/cameraFollow.cs
@@ -5,23 +5,13 @@ using UnityEngine;
 public class cameraFollow : MonoBehaviour
 {
   public GameObject target;
-  public float damping = 1f;
-  Vector3 offset;
-
-  void Start()
-  {
-    offset = target.transform.position - transform.position;
-  }
+  public float followDistance = 1f;
+  public float cameraHeight = 10f;
 
   void LateUpdate()
   {
-    float currentAngle = transform.eulerAngles.y;
-    float desiredAngle = target.transform.eulerAngles.y;
-    float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
-
-    Quaternion rotation = Quaternion.Euler(0, angle, 0);
-    transform.position = target.transform.position - (rotation * offset);
-
-    transform.LookAt(target.transform);
+    transform.position = new Vector3(target.transform.position.x + followDistance,
+                                          target.transform.position.y + cameraHeight,
+                                          target.transform.position.z + followDistance);
   }
 }
