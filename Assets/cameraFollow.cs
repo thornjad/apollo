d078cd75802b87e99509f9d7144ca17e60873fb5
Jade Thornton <jade@jmthornton.net>
2017-01-13 20:45:25 -0600

    Add very basic camera follow functionality

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+  public GameObject target;
+  public float followDistance = 1f;
+  public float cameraHeight = 10f;
+
+  void LateUpdate()
+  {
+    transform.position = new Vector3(target.transform.position.x + followDistance,
+                                          target.transform.position.y + cameraHeight,
+                                          target.transform.position.z + followDistance);
+  }
+}
