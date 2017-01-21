4cdd226371058eedd49975331b19a99efd2dc905
Jade Thornton <jade@jmthornton.net>
2017-01-20 22:44:05 -0600

    Fix deprecated code from issue #2

--- Assets/Standard Assets/Utility/ParticleSystemDestroyer.cs
+++ Assets/Standard Assets/Utility/ParticleSystemDestroyer.cs
@@ -14,8 +14,8 @@ namespace UnityStandardAssets.Utility
         public float minDuration = 8;
         public float maxDuration = 10;
 
-        private float m_MaxLifetime;
-        private bool m_EarlyStop;
+        private float MaxLifetime;
+        private bool StopEarly;
 
 
         private IEnumerator Start()
@@ -23,16 +23,15 @@ namespace UnityStandardAssets.Utility
             var systems = GetComponentsInChildren<ParticleSystem>();
 
             // find out the maximum lifetime of any particles in this effect
-            foreach (var system in systems)
-            {
-                m_MaxLifetime = Mathf.Max(system.startLifetime, m_MaxLifetime);
+            foreach (var system in systems) {
+                MaxLifetime = Mathf.Max(system.main.startLifetime.constant, MaxLifetime);
             }
 
             // wait for random duration
 
             float stopTime = Time.time + Random.Range(minDuration, maxDuration);
 
-            while (Time.time < stopTime || m_EarlyStop)
+            while (Time.time < stopTime || StopEarly)
             {
                 yield return null;
             }
@@ -47,7 +46,7 @@ namespace UnityStandardAssets.Utility
             BroadcastMessage("Extinguish", SendMessageOptions.DontRequireReceiver);
 
             // wait for any remaining particles to expire
-            yield return new WaitForSeconds(m_MaxLifetime);
+            yield return new WaitForSeconds(MaxLifetime);
 
             Destroy(gameObject);
         }
@@ -56,7 +55,7 @@ namespace UnityStandardAssets.Utility
         public void Stop()
         {
             // stops the particle system early
-            m_EarlyStop = true;
+            StopEarly = true;
         }
     }
 }
